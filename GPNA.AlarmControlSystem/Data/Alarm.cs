ef9ac7f546a15219459079fa63bf9d31c64135ba
Poletaev Stepan <poletaev.21@gmail.com>
2022-03-22 03:57:35 +0300

    21.03.2022

---
+++ GPNA.AlarmControlSystem/Data/Alarm.cs
@@ -0,0 +1,31 @@
+﻿namespace GPNA.AlarmControlSystem.Data
+{
+    public class Alarm
+    {
+        public DateTime DateTime {get;set;}
+        public string Date => DateTime.Date.ToString("d");
+        public string Time => DateTime.Date.ToString("T");        
+        public string? ConditionName { get; set; }
+        public string? Source { get; set; }
+        public string? Description { get;set;}
+
+        public int Priority 
+        {
+            get
+            {
+                if (ConditionName == "PVLL")
+                    return 1;
+                if (ConditionName == "PVLO" || ConditionName == "RSLO")
+                    return 2;
+                if (ConditionName == "PVHI")
+                    return 3;
+                if (ConditionName == "PVHH")
+                    return 4;
+                if (ConditionName == "ALARM")
+                    return 5;
+                else
+                    return 0;
+            }
+        }
+    }
+}
---
+++ GPNA.AlarmControlSystem/Data/AlarmService.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace GPNA.AlarmControlSystem.Data
+{
+    public class AlarmService
+    {
+        
+        public Task<Alarm[]> GetAlarmsAsync()
+        {
+            var obj = Task.FromResult(JsonSerializer.Deserialize<Alarm[]>(File.ReadAllText("Alarms.json")));
+            return obj!;
+        }
+    }
+}
