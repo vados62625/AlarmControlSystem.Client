7f65d9ff31bc40f7cfe71ab015d619ea7f7b9146
poletaev21 <poletaev.21@gmail.com>
2022-12-05 14:57:06 +0300

    page Archive

--- GPNA.AlarmControlSystem/Pages/Reports/Reports.razor.cs
+++ GPNA.AlarmControlSystem/Pages/Reports/Reports.razor.cs
@@ -27,36 +27,61 @@ namespace GPNA.AlarmControlSystem.Pages.Reports
         {
             To = DateTime.Now.AddDays(-1);
             To = new DateTime(To.Year, To.Month, To.Day, 23, 59, 59);
-            From = DateTime.Now.AddDays(-7);
-            From = new DateTime(From.Year, From.Month, From.Day, 0, 0, 0);
+            From = DateTime.Now.AddDays(-1);
+            From = new DateTime(From.Year, From.Month, From.Day, 0, 0, 0);            
         }
 
         protected override async Task OnParametersSetAsync()
         {
             IsEnableRenderChart = false;
             SpinnerService.Show();
+            StateHasChanged();
             GeneralCount = 0;
-            AverageUrgent = 0;
-            IncomingAlarms = await IncomingAlarmService.GetCountInHour(1, From, To);                       
+            AverageUrgent = SetAverageUrgent().Result;
+            IncomingAlarms = await IncomingAlarmService.GetCountInHour(1, From, To);
 
             foreach (var alarmsOnHour in IncomingAlarms)
             {
-                if (alarmsOnHour.Value!=null && alarmsOnHour.Value.Count > 0)
-                    foreach (var alarm in alarmsOnHour.Value)
-                    {
-                        GeneralCount += 1;
-                        AlarmsCountArm1 += 1;
-                        if (alarm.Priority == PriorityType.Urgent)
-                        {
-                            AverageUrgent += 1;
-                        }
-                    }                
+                if (alarmsOnHour.Value != null && alarmsOnHour.Value.Count > 0)
+                { 
+                    GeneralCount += alarmsOnHour.Value.Count;
+                    AlarmsCountArm1 += alarmsOnHour.Value.Count;                        
+                }
             }
-
-            AverageUrgent = AverageUrgent / IncomingAlarms.Count;
 
             SpinnerService.Hide();
             IsEnableRenderChart = true;
+            StateHasChanged();
+        }
+
+        async void SetDateTime(int days)
+        {
+            To = DateTime.Now.AddDays(-1);
+            To = new DateTime(To.Year, To.Month, To.Day, 23, 59, 59);
+            From = DateTime.Now.AddDays(-days);
+            From = new DateTime(From.Year, From.Month, From.Day, 0, 0, 0);
+
+            await OnParametersSetAsync();
+        }
+
+        async Task<int> SetAverageUrgent()
+        {
+            var incomingAlarms = await IncomingAlarmService.GetCountInHour(1, DateTime.Now.AddHours(-720), DateTime.Now);
+            int countUrgent = 0;
+            foreach (var alarmsOnHour in incomingAlarms)
+            {
+                if (alarmsOnHour.Value != null && alarmsOnHour.Value.Count > 0)
+                    foreach (var alarm in alarmsOnHour.Value)
+                    {                        
+                        if (alarm.Priority == PriorityType.Urgent)
+                        {
+                            countUrgent += 1;
+                        }
+                    }
+            }
+            if (incomingAlarms.Count > 0)
+                return countUrgent / incomingAlarms.Count;
+            return -1;
         }
     }
 }
