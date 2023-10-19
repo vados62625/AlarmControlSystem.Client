6749105f03f884b0894a25eb698cf61f15956154
Вадим Имморталем <vados62625@gmail.com>
2023-10-19 20:29:56 +0300

    added charts + added tables data

--- GPNA.AlarmControlSystem/Models/Dto/Workstation/WorkstationMainPageDto.cs
+++ GPNA.AlarmControlSystem/Models/Dto/Workstation/WorkstationMainPageDto.cs
@@ -5,7 +5,9 @@ namespace GPNA.AlarmControlSystem.Models.Dto.Workstation;
 public class WorkstationMainPageDto
 {
     public string Name { get; set; }
-    public Dictionary<DateTimeOffset, CountAlarmsOnPriority[]> CountAlarmsOnPrioritiesMap { get; set; } = new();
+    public Dictionary<DateTimeOffset, CountAlarmsOnPriority[]> CountIncomingAlarms { get; set; } = new();
+    public Dictionary<DateTimeOffset, CountAlarmsOnPriority[]> CountActiveAlarms { get; set; } = new();
+    public Dictionary<DateTimeOffset, CountAlarmsOnPriority[]> CountSuppressedAlarms { get; set; } = new();
     public bool Fire { get; set; } = false;
     public bool Gas { get; set; } = false;
     public int TotalAlarmsCount { get; set; }
