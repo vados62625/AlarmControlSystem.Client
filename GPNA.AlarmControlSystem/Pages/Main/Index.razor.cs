9b3a61645b6ec33e3110a3df386f02cd79510036
Вадим <vados62625@gmail.com>
2023-11-08 11:51:05 +0300

    added additional files to pages index and monitoring

---
+++ GPNA.AlarmControlSystem/Pages/Main/Index.razor.cs
@@ -0,0 +1,99 @@
+﻿using GPNA.AlarmControlSystem.Interfaces;
+using GPNA.AlarmControlSystem.Models.Dto.Field;
+using GPNA.AlarmControlSystem.Models.Dto.Queries;
+using GPNA.AlarmControlSystem.Models.Dto.Workstation;
+using GPNA.AlarmControlSystem.Options;
+using GPNA.AlarmControlSystem.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Options;
+
+namespace GPNA.AlarmControlSystem.Pages.Main;
+
+public partial class Index : ComponentBase
+{
+    [Inject]
+    private IOptions<AcsModuleOptions>? Options { get; set; }
+    
+    [Inject]
+    private IFieldService? FieldService { get; set; }
+
+    [Inject]
+    private IWorkStationService? WorkStationService { get; set; }
+
+    [Inject]
+    protected ISpinnerService SpinnerService { get; set; } = default!;
+
+    [Parameter]
+    public DateTimeOffset DateTime { get; set; } = DateTimeOffset.Now.AddDays(-1);
+
+    [Parameter]
+    [SupplyParameterFromQuery]
+    public int? FieldId { get; set; }
+
+    private bool IsEnableRenderChart { get; set; }
+
+    private FieldDto[]? _fields;
+
+    private WorkstationMainPageDto[]? _workstations;
+
+    private IDictionary<string, string>? ArmLinksDictionary { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        IsEnableRenderChart = false;
+        SpinnerService.Show();
+
+        await InitializePageAsync();
+
+        SpinnerService.Hide();
+        IsEnableRenderChart = true;
+
+        await base.OnInitializedAsync();
+    }
+
+    private async Task InitializePageAsync()
+    {
+        await GetFields();
+
+        FieldId ??= _fields?.FirstOrDefault()?.Id ?? 1;
+
+        await GetWorkstations();
+
+        FillArmLinks();
+    }
+
+    private async Task GetFields()
+    {
+        if (FieldService != null)
+        {
+            var result = await FieldService.GetList();
+
+            if (result.Success)
+            {
+                _fields = result.Payload.ToArray();
+            }
+        }
+    }
+
+    private async Task GetWorkstations()
+    {
+        if (WorkStationService != null && FieldId != null)
+        {
+            var query = new GetAlarmsCountForFieldQuery { FieldId = FieldId.Value, ActivationFrom = DateTime.AddDays(-7), ActivationTo = DateTime };
+            var result = await WorkStationService.GetWorkstationsWithStatistics(query);
+
+            if (result.Success)
+            {
+                _workstations = result.Payload.ToArray();
+            }
+        }
+    }
+
+    private void FillArmLinks()
+    {
+        if (_fields != null && _fields.Any())
+        {
+            ArmLinksDictionary = _fields.ToDictionary(field => field.Name, field => $"/?fieldId={field.Id}");
+        }
+    }
+}
