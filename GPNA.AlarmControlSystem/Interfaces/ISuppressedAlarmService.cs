c39c86d2c3f547454ffecd99faccb57a988bf97a
Вадим Имморталем <vados62625@gmail.com>
2023-11-16 23:03:16 +0300

    added SuppressedAlarms to monitoring

--- GPNA.AlarmControlSystem/Interfaces/ISuppressedAlarmService.cs
+++ GPNA.AlarmControlSystem/Interfaces/ISuppressedAlarmService.cs
@@ -1,5 +1,7 @@
 ﻿using GPNA.AlarmControlSystem.Models.Dto;
 using GPNA.AlarmControlSystem.Models.Dto.ActiveAlarm;
+using GPNA.AlarmControlSystem.Models.Dto.BufferAlarms;
+using GPNA.AlarmControlSystem.Models.Dto.IncomingAlarm;
 using GPNA.AlarmControlSystem.Models.Dto.SuppressedAlarm;
 using GPNA.RestClient.Interfaces.Brokers;
 using GPNA.RestClient.Models;
@@ -9,5 +11,6 @@ namespace GPNA.AlarmControlSystem.Interfaces
     public interface ISuppressedAlarmService : ICrudBase<SuppressedAlarmDto>
     {
         Task<Result<CountAlarmsOnDate[]>> GetCountSuppressedAlarmsByDates(GetCountSuppressedAlarmsByDatesQuery content);
+        Task<Result<AlarmsCollection<SuppressedAlarmDto>>> GetSuppressedAlarmsPerDate(GetIncomingAlarmsByDatesQuery content);
     }
 }
