f42d8b4cf67a976b644489495da29dcd6bbaf7b6
poletaev21 <poletaev.21@gmail.com>
2023-11-29 10:55:48 +0300

    привязал модалку для перевода сигналищации в задачи. добавил snackbar

--- GPNA.AlarmControlSystem/wwwroot/scripts.js
+++ GPNA.AlarmControlSystem/wwwroot/scripts.js
@@ -1,5 +1,9 @@
-window.saveComment = (id) => {
+window.saveComment = (id, text) => {
     var x = document.getElementById("snackbar");
+    if (text) 
+    {
+        x.textContent = text;
+    }
     x.className = "show";
     setTimeout(function () { x.className = x.className.replace("show", ""); }, 2000);
 }
