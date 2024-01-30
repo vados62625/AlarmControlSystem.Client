53d67dcdf36c3bd4f189b94ee20ea2e98255093c
Вадим <vados62625@gmail.com>
2024-01-30 16:44:24 +0300

    added TagChangeDto

---
+++ GPNA.AlarmControlSystem/Models/Dto/TagChange/TagChangeDto.cs
@@ -0,0 +1,37 @@
+﻿using GPNA.AlarmControlSystem.Models.Dto;
+using GPNA.AlarmControlSystem.Models.Dto.Tag;
+
+namespace GPNA.AlarmControlSystem.Application.Dto.TagChange
+{
+    /// <summary>
+    /// Задача
+    /// </summary>
+    public class TagChangeDto : EntityDtoBase
+    {
+        /// <summary>
+        /// Id тега
+        /// </summary>
+        public int TagId { get; set; }
+        
+        public TagDto? Tag { get; set; }
+
+        /// <summary>
+        /// Id инициатора
+        /// </summary>
+        public int InitiatorId { get; set; }
+
+        public virtual UserDto? Initiator { get; set; }
+
+        /// <summary>
+        /// Id исполнителя
+        /// </summary>
+        public int ExecutorId { get; set; }
+
+        public virtual UserDto? Executor { get; set; }
+
+        /// <summary>
+        /// В архиве
+        /// </summary>
+        public bool Archived { get; set; }
+    }
+}
---
+++ GPNA.AlarmControlSystem/Models/Dto/TagChange/TagChangesCollection.cs
@@ -0,0 +1,16 @@
+﻿namespace GPNA.AlarmControlSystem.Application.Dto.TagChange;
+
+public class TagChangesCollection
+{
+    public TagChangeDto[] Items { get; set; } = Array.Empty<TagChangeDto>();
+    
+    public int PagesCount { get; set; } = 1;
+
+    public int TotalCount { get; set; }
+
+    public int IncorrectDescription { get; set; }
+    
+    public int IncorrectPriority { get; set; }
+    
+    public int IncorrectAlarmLimit { get; set; }
+}
